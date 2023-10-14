956d358190f7e3d1b03b55c924512ab9f20c9b79
romariosilva <1994romariosilvanunes@gmail.com>
2023-10-14 11:55:21 -0300

    atualizando

---
+++ Projeto-Iphone/src/interfaceIphone/executarOpcao/opcoes.java
@@ -0,0 +1,53 @@
+package interfaceiPhone.executarOpcao;
+
+
+
+import interfaceiPhone.aparelho.aparelhoTelefonico;
+import interfaceiPhone.navegador.navegadorInternet;
+import interfaceiPhone.reprodutor.reprodutorMusicas;
+
+public class opcoes implements aparelhoTelefonico,reprodutorMusicas,navegadorInternet{
+    
+    public opcoes(){}
+    
+    @Override  
+    public void ligar(){
+        System.out.println("\nFazendo  Ligação....\n");
+    }
+    @Override
+    public void atender(){
+        System.out.println("\nAtendendo Ligação...\n");
+    }
+    @Override
+    public void iniciarCorrerioVoz(){
+        System.out.println("\nIniciando Chamada do Correio de Voz...\n");
+    }
+
+    @Override
+    public void tocar(){
+        System.out.println ("\nTocando Música... \n");
+    }
+    @Override
+    public void pausar(){
+        System.out.println("\nPausando Música...\n");
+    }
+    @Override
+    public void selecionarMusica(){
+        System.out.println("\nSelecionando Músicas...\n");
+    }
+
+    @Override
+    public void exibirPagina(){
+        System.out.println("\nExibindo Página Aberta...\n");
+    }
+    @Override
+    public void adicionarNovaAba(){
+        System.out.println("\nAdicionar Nova Aba....\n"); 
+    }
+    @Override
+    public void atualizarPagina(){
+        System.out.println("\nAtualizando Página...\n"); 
+    }
+
+   
+}
---
+++ Projeto-Iphone/src/interfaceIphone/navegador/navegadorInternet.java
@@ -0,0 +1,11 @@
+package interfaceiPhone.navegador;
+
+public interface navegadorInternet   {
+
+    public void exibirPagina();
+
+    public void adicionarNovaAba();
+
+    public void atualizarPagina();
+    
+}
