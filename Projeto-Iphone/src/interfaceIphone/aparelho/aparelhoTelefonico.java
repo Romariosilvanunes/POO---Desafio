c9bcacd717deb9be776913f5ec517eacecf738e8
romariosilva <1994romariosilvanunes@gmail.com>
2023-10-14 11:37:52 -0300

    .

---
+++ Projeto-Iphone/src/interfaceIphone/aparelho/aparelhoTelefonico.java
@@ -0,0 +1,15 @@
+package interfaceiPhone.aparelho;
+
+public interface aparelhoTelefonico {
+
+   
+
+    public void ligar();
+
+    public void atender();
+
+    public void iniciarCorrerioVoz();
+    
+  
+    
+}
